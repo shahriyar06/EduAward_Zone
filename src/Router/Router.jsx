3d6f4298e43b4df84f70572e8526e00eca147c47
Shahriyar <chyshahriyar@gmail.com>
2024-06-15 12:17:34 +0600

    Edu Award Zone router, tailwind, daisi ui and some page set up completed

---
+++ src/Router/Router.jsx
@@ -0,0 +1,24 @@
+import {
+    createBrowserRouter
+} from "react-router-dom";
+import Root from "../Root/Root";
+import Error from "../Page/Error/Error";
+import Home from "../Page/Home/Home";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        errorElement: <Error></Error>,
+        children: [
+            {
+                path: '/home',
+                element: <Home></Home>
+            }
+        ],
+    },
+]);
+
+export default router;
