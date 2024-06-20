3cccdb969b9a92d303354badd899ca327e3fd5a8
Shahriyar <chyshahriyar@gmail.com>
2024-06-21 00:33:09 +0600

    user delete successfully

--- src/Page/AllUser/AllUser.jsx
+++ src/Page/AllUser/AllUser.jsx
@@ -1,46 +1,76 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../Hook/useAxiosSecure";
+import { FaTrash } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 
 const AllUser = () => {
 
     const axiosSecure = useAxiosSecure();
-    const { data: users = [] } = useQuery({
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
             return res.data;
         }
     })
+    const handledelete = (user) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${user._id}`,)
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: `${user.name} has been deleted.`,
+                                icon: "success"
+                            });
+                            refetch();
+                        }
+                    })
+            }
+        });
+    }
 
     return (
         <div className="overflow-x-auto">
-  <table className="table table-zebra">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>Photo</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Role</th>
-        <th>Details</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        users.map((user, index) => <tr key={user._id}>
-        <th>{index+1}</th>
-        <td><img src={user.photo} className="w-14 h-14 rounded-xl" alt="" /></td>
-        <td>{user.name}</td>
-        <td>{user.email}</td>
-        <td className="btn">Details</td>
-        <td>{user.email}</td>
-      </tr>)
-      }
-    </tbody>
-  </table>
-</div>
+            <table className="table table-zebra">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Photo</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th></th>
+                        <th>Details</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user, index) => <tr key={user._id}>
+                            <th>{index + 1}</th>
+                            <td><img src={user.photo} className="w-14 h-14 rounded-xl" alt="" /></td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td className="btn">Details</td>
+                            <td></td>
+                            <td className="btn btn-outline text-[#D2093C] hover:bg-[#D2093C]"><FaTrash className="text-xl" /></td>
+                        </tr>)
+                    }
+                </tbody>
+            </table>
+        </div>
     );
 };
 
